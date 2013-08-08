c4507f0cb3cde69cebabb0baeeb3a7f25f5de64d
Mark Thompson <mark2thompson@gmail.com>
2013-08-08 18:50:50 -0400

    finished last video

--- week1/gen/com/example/week1/R.java
+++ week1/gen/com/example/week1/R.java
@@ -24,15 +24,26 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f070000;
+        public static final int action_settings=0x7f080000;
+    }
+    public static final class integer {
+        public static final int dime=0x7f040001;
+        public static final int nickel=0x7f040002;
+        public static final int penny=0x7f040003;
+        public static final int quarter=0x7f040000;
     }
     public static final class menu {
-        public static final int main=0x7f060000;
+        public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f040001;
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040002;
+        public static final int action_settings=0x7f050001;
+        public static final int app_name=0x7f050000;
+        public static final int dime=0x7f050005;
+        public static final int hello_world=0x7f050002;
+        public static final int moneyamts=0x7f050003;
+        public static final int nickel=0x7f050006;
+        public static final int penny=0x7f050007;
+        public static final int quarter=0x7f050004;
     }
     public static final class style {
         /** 
@@ -56,10 +67,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
