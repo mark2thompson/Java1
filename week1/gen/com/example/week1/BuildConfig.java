8b3b3ae3c5a4eb03c6caee098630a738803065e8
Mark Thompson <mark2thompson@gmail.com>
2013-08-08 09:44:45 -0400

    First comment for the project

---
+++ week1/gen/com/example/week1/BuildConfig.java
@@ -0,0 +1,9 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.week1;
+
+/**
+ * The Class BuildConfig.
+ */
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
